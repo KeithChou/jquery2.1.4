011bfe5fe1ab71c476b44906091e6d67464b0beb
keithchou <keithchou95@gmail.com>
2017-05-04 23:03:05 +0800

    jquery的一些通用函数,增加isArraylike

---
+++ common/util.js
@@ -0,0 +1,25 @@
+/*
+* jQuery-2.1.4 reading by unclekeith
+* isArraylike
+*/
+
+function isArraylike(obj) {
+	// 这里的一些算法主要是判断obj是否是Array或者是
+	// Arraylike
+	// $.type 是用于取得object的javascript类
+	// $.isWindow 用于确定object是否是浏览器窗口
+	// $.type，$.isWindow比较少用到，暂时不做分析
+	var length = "length" in obj && obj.length,
+		type = jQuery.type(obj);
+
+	if (type === "function" || jQuery.isWindow(obj)) {
+		return false;
+	}
+
+	if (obj.nodeType === 1 && length) {
+		return true;
+	}
+
+	return type === "array" || length === 0 ||
+		typeof length === "number" && length > 0 && (length - 1) in obj;
+}
