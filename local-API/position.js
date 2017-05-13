7a04a068a4ec73c2075ad53cfc7c106dd6b3b65c
keithchou <keithchou95@gmail.com>
2017-05-13 15:09:18 +0800

    增加appendTo,prependTo方法的分析

--- local-API/position.js
+++ local-API/position.js
@@ -1,6 +1,6 @@
 /*
 * jQuery-2.1.4 reading by unclekeith
-* offset
+* offset, appendTo, prependTo, insertBefore, insertAfter, replaceALL
 */
 
 $.fn.extend({
@@ -49,8 +49,69 @@ $.fn.extend({
 		}
 		win = getWindow(doc);
 		return {
+			// 这里添加了win.pageYOffset和docElem.clientTop
+			// 估计是考虑getBoundingClientRect方法的浏览器兼容性问题
 			top: box.top + win.pageYOffset - docElem.clientTop,
 			left: box.left + win.pageXOffset - docElem.clientLeft
 		};
 	}
+});
+
+$.fn.extend({
+	append: function() {
+		// 以appendTo方法举例
+		// 1. $('<p>keith</p>').appendTo('body');
+		// 1. arguments指的就是P节点
+
+		// 这里简单的说以下domManip函数的原理
+		// 在正常使用domManip的情况下，arguments对象包含一些需要添加到this对象的节点
+		// 并且在内部实现中，jQuery封装了buildFragment方法
+		// 在buildFragment方法中，使用了document对象的DocumentFragment方法，也就是说通常所说的'文档碎片'
+		// 将需要插入的元素先插入到文档碎片中，然后全部操作完毕，再将文档碎片插入到指定元素中，而文档碎片自身并不会被插入
+		// 使用文档碎片的好处是可以离线操作DOM，最大程度减少页面的重排和重绘，提高web性能
+		// 所以在appendTo、append，prependTo、prepend等插入节点的方法中都采用了离线操作DOM的方式
+		return this.domManip(arguments, function(elem) {
+			// nodeType=1表示this对象是一个元素节点
+			// nodeType=11表示this对象是一个文档碎片
+			// nodeType=9表示this对象是一个document对象
+			if (this.nodeType === 1 || this.nodeType === 11 || this.nodeType === 9) {
+				var target = manipulationTarget(this, elem);
+				target.appendChild(elem);
+			}
+		});
+	}
 })
+
+jQuery.each({
+	appendTo: "append",
+	prependTo: "prepend",
+	insertBefore: "before",
+	insertAfter: "after",
+	replaceAll: "replaceWith"
+}, function(name, original) {
+	// 以appendTo方法举例
+	// 1. $('<p>keith</p>').appendTo('body');
+	// $.fn.appendTo扩展了jQuery对象的appendt方法
+	// appendTo方法接收一个参数，表示要插入到的selector
+	jQuery.fn[name] = function(selector) {
+		var elems,
+			ret = [],
+			insert = jQuery(selector),
+			last = insert.length - 1,
+			i = 0;
+
+		for (; i <= last; i++) {
+			elems = i === last ? this : this.clone(true);
+			// appendTo, prependTo, insertBefore, insertAfter, replaceAll都调用了
+			// 它们对应的方法apend, prepend, before, after, replaceWith
+			// 1. insert[i] = 'body'; original = 'append'; elems = P节点
+			jQuery(insert[i])[original](elems);
+
+			// Support: QtWebKit
+			// .get() because push.apply(_, arraylike) throws
+			push.apply(ret, elems.get());
+		}
+
+		return this.pushStack(ret);
+	};
+});
